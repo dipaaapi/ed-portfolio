aa0a332ac6a12fcabfcf8442fe1b10e7beeaa47a
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2022-03-05 22:16:21 +0800

    remove link at navigation

--- components/nav.tsx
+++ components/nav.tsx
@@ -9,14 +9,10 @@ const Navigation = () => (
                     <Link href="/">
                         <a className=''>
                             <Image src="/CyberEd.gif" alt="Edward Logo" width={50} height={50} className='flex-shrink rounded' />
-                            {/* <div className="flex-grow bg-gradient-to-r from-red-500 to-dark p-2 text-left rounded">
-                                <p className="text-white text-lg font-black">CyberEd</p>
-                                <p className="text-white text-xs">Leads you to your future</p>
-                            </div> */}
                         </a>
                     </Link>
                 </li>
-                <div className='md:flex hidden space-x-2 text-dark'>
+                {/* <div className='md:flex hidden space-x-2 text-dark'>
                     <li className='hover:text-red-500 font-semibold'>
                         <Link href="/education/first">
                         <a>First Post</a>
@@ -27,7 +23,7 @@ const Navigation = () => (
                         <a>Second Post</a>
                         </Link>
                     </li>
-                </div>
+                </div> */}
             </ul>
         </nav>
     </div>
