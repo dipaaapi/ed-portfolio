df9201101f98e163a2102f9a3209b1d99df10989
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2022-03-11 22:44:41 +0800

    update work exp page and add work logos

--- pages/workxp.tsx
+++ pages/workxp.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Navigation from '../components/nav';
 import Footer from '../components/footer'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image';
 
 const workxp = () => {
     return (
@@ -11,26 +12,52 @@ const workxp = () => {
                 <link rel="icon" href="/CyberEd.gif" />
             </Head>
             <Navigation />
-
             <main>
                 <h1 className='text-2xl uppercase text-red-500 font-bold my-3 text-center'>Work Experience</h1>
-                <div className="w-full bg-whiteg">
-                    <div className="w-10/12 mx-auto p-5 group font-main relative">
-                        <p className="text-red-500 text-2xl group-hover:translate-x-[60%] group-hover:text-blueg transition-all delay-100 p-3">Digima Web Solutions Inc.</p>
-                        <p className="absolute bottom-0 left-[3] transition-all delay-100 -z-10 group-hover:z-10 text-left hidden text-sm text-red-500 p-3 group-hover:block">June 18, 2018 - August 30, 2019</p>
-                    </div>
-                    <div className="w-10/12 mx-auto p-5 group font-main">
-                        <p className="text-red-500 text-2xl group-hover:translate-x-[60%] group-hover:text-blueg transition-all delay-100 p-3">Vanity Health and Wellness</p>
-                    </div>
-                    <div className="w-10/12 mx-auto p-5 group font-main">
-                        <p className="text-red-500 text-2xl group-hover:translate-x-[60%] group-hover:text-blueg transition-all delay-100 p-3">Vertex Digital Entertainment Technologies Inc.</p>
-                    </div>
-                    <div className="w-10/12 mx-auto p-5 group font-main">
-                        <p className="text-red-500 text-2xl group-hover:translate-x-[60%] group-hover:text-blueg transition-all delay-100 p-3 group-hover:drop-shadow-md">AiPayGo Inc.</p>
+                <div className="w-full bg-whiteg p-5">
+                    <div className="w-10/12 md:w-2/3 mx-auto grid grid-cols-1 md:grid-cols-3 gap-3 p-5">
+                        <div className="relative w-10/12 md:w-[20vw] group bg-white rounded group-hover:shadow-lg shadow mx-auto p-5 group font-main">
+                            <div className="absolute left-0 top-5 z-10 opacity-100 group-hover:opacity-0 group-hover:transition-all group-hover:delay-500 w-full">
+                                <div className="w-max mx-auto">
+                                    <Image src="/digima_logo_square.png" alt="digima logo" width={100} height={100} className="w-full" />
+                                </div>
+                            </div>
+                            <div className="group-hover:opacity-100 opacity-0 group-hover:transition-all group-hover:delay-500 w-full">
+                                <div className="w-max mx-auto">
+                                    <Image src="/digima_logo_landscape.png" alt="digima logo" width={200} height={75} className="w-full" />
+                                </div>
+                                <p className="text-left text-sm text-red-500 p-3 group-hover:block">June 18, 2018 - August 30, 2019</p>
+                            </div>
+                        </div>
+                        <div className="relative w-10/12 md:w-[20vw] group bg-white rounded group-hover:shadow-lg shadow mx-auto p-5 group font-main">
+                            <div className="absolute left-0 top-5 z-10 opacity-100 group-hover:opacity-0 group-hover:transition-all group-hover:delay-500 w-full">
+                                <div className="w-max mx-auto">
+                                    <Image src="/vanity_logo_square.png" alt="vanity logo" width={100} height={100} className="w-full" />
+                                </div>
+                            </div>
+                            <div className="group-hover:opacity-100 opacity-0 group-hover:transition-all group-hover:delay-500 w-full">
+                                <div className="w-max mx-auto">
+                                    <Image src="/vanity_logo_landscape.png" alt="vanity logo" width={200} height={75} className="w-full" />
+                                </div>
+                                <p className="text-left text-sm text-red-500 p-3 group-hover:block">January 2020 - March 2020</p>
+                            </div>
+                        </div>
+                        <div className="relative w-10/12 md:w-[20vw] group bg-white rounded group-hover:shadow-lg shadow mx-auto p-5 group font-main">
+                            <div className="absolute left-0 top-5 z-10 opacity-100 group-hover:opacity-0 group-hover:transition-all group-hover:delay-500 w-full">
+                                <div className="w-max mx-auto">
+                                    <Image src="/vertex_logo_square.png" alt="vertex logo" width={100} height={100} className="w-full" />
+                                </div>
+                            </div>
+                            <div className="group-hover:opacity-100 opacity-0 group-hover:transition-all group-hover:delay-500 w-full">
+                                <div className="w-max mx-auto">
+                                    <Image src="/vertex_logo_landscape.png" alt="vertex logo" width={200} height={75} className="w-full" />
+                                </div>
+                                <p className="text-left text-xs text-red-500 p-3 group-hover:block">May 26, 2020 - November 30, 2020</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </main>
-
             <Footer />
         </div>
     );
