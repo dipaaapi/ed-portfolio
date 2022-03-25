caa6976b34fa2a4135e68993528287ace1e93420
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2022-03-25 23:29:40 +0800

    tesda info added

--- pages/education.tsx
+++ pages/education.tsx
@@ -7,8 +7,8 @@ import styles from '../styles/Home.module.css'
 const education = () => {
     const info = {
         level: ['primary', 'secondary', 'vocational', 'vocational', 'tertiary'],
-        title: ['amado v. hernandez', 'dr. juan g. nolasco', 'iscom', 'technical education and skills development authority', 'informatics college manila'],
-        status: ['elementary school', 'high school', 'computer literacy basic and advance', 'computer hardware servicing / computer technician', 'bachelor of science in information tecnology (bsit)'],
+        title: ['amado v. hernandez', 'dr. juan g. nolasco', 'iscom', 'TESDA', 'informatics college manila'],
+        status: ['elementary school', 'high school', 'computer literacy basic and advance', 'CHS or CSS / Comp. Tech.', 'bachelor of science in information tecnology (bsit)'],
         graduate: ['april 25, 2001', 'april 13, 2005', 'june 8, 2008', 'july 16, 2012', 'april 28, 2018'],
         address: ['sto. niño street, barrio magsaysay, tondo, sto. niño street, 107, tondo, manilla, 1012 metro manila', '#252 tioco, tondo, maynila, kalakhang maynila', 'plaza morga corner moriones st., tondo, manila', '#233 lopez bldg. tayuman street tondo, manila', '2070 bdo building c.m. recto, manila philippines']
     }
@@ -55,6 +55,17 @@ const education = () => {
                         <div className="my-auto">
                             <p className="text-3xl text-left font-bold text-white uppercase">{info.status[2]}</p>
                             <p className="text-2xl text-left font-bold text-white uppercase">{info.level[2]}</p>
+                        </div>
+                    </div>
+                    <div className="w-max mx-auto flex p-5 space-x-5">
+                        <div className="my-auto">
+                            <p className="text-3xl text-right font-bold text-white uppercase">{info.title[3]}</p>
+                            <p className="text-2xl text-right font-bold text-white uppercase">{info.graduate[3]}</p>
+                        </div>
+                        <img src="/tesda.png" alt="tesda" className="w-[10vw] hover:animate-ping" />
+                        <div className="my-auto">
+                            <p className="text-3xl text-left font-bold text-white uppercase">{info.status[3]}</p>
+                            <p className="text-2xl text-left font-bold text-white uppercase">{info.level[3]}</p>
                         </div>
                     </div>
                 </div>
