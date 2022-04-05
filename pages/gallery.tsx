f629506b674cad1d876e81c21801a3e9a5960873
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2022-04-05 21:24:28 +0800

    udate gallery page display uniqueness

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,7 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
 import Navigation from "../components/nav";
 import Footer from "../components/footer";
-import Image from "next/image";
 export const getStaticProps = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/photos');
     const data = await res.json();
@@ -18,20 +18,17 @@ const gallery = ({ images }) => {
                 <link rel="icon" href="/CyberEd.gif" />
             </Head>
             <Navigation />
-            <div className="mt-10">
+            <div className="mt-10 grid grid-cols-2 md:grid-cols-4 gap-3 w-10/12 md:w-2/3 mx-auto">
                 {images.map(images => (
-                    <div key={images.id} className="w-2/3 mx-auto">
-                        <a className="block md:my-0 my-5 md:border-y-0 border-y md:py-0 py-2 px-4 hover:shadow-blue-500 rounded-full hover:bg-gray-200 justify-center md:justify-between">
-                            <p className="md:py-2 md:px-4 hover:cursor-pointer rounded-full md:bg-none hover:bg-blue-600 hover:text-white md:my-5 font-bold md:text-left text-center uppercase text-sm md:text-base">{ images.title }</p>
-                            <Image src="/{ images.url }" alt={ images.id } width={150} height={150} className="my-10 mx-auto" />
-                        </a>
+                    <div key={images.id} className="">
+                        <img src={ images.thumbnailUrl } alt={ images.title } className="w-[150px] h-[150px] mx-auto" />
+                        <p className="text-xs italic text-center">{ images.title }</p>
                     </div>
                 ))}
-                <div className="w-full bg-blue-600">
-                    <div className="w-max py-2 mt-5 mx-auto">
-                        <i className="my-auto font-semibold text-sm text-white">source: JSONPlaceholder</i>
-                    </div>
-
+            </div>
+            <div className="w-full bg-blue-600">
+                <div className="w-max py-2 mt-5 mx-auto">
+                    <i className="my-auto font-semibold text-sm text-white">source: JSONPlaceholder</i>
                 </div>
             </div>
             <Footer />
