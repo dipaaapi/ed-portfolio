ce759e7ffcd20aa21580cad6788c76e7f3c0884c
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2023-02-11 13:35:15 +0800

    update hobbies and skill page

--- pages/hobbies.tsx
+++ pages/hobbies.tsx
@@ -6,10 +6,12 @@ import Image from 'next/image'
 import Head from 'next/head'
 
 const Hobbies: NextPage = () => {
-    // function anything() {
-    //     createCanvas(200, 200);
-    //     loadJSON = ('http://api.weatherapi.com/v1/current.json?key=97f90c2ace4a4e8599a62434220304&q=Philippines&aqi=no', gotData);
-    // }
+    const importanceHobbies = [
+        { title: "Playing Video Games", description: `Playing video games is a great hobby that can provide a source of entertainment, challenge, and relaxation. It's a great way to unwind after a long day and can also improve your hand-eye coordination, problem-solving skills, and decision-making abilities. In the Philippines, video gaming has become a popular pastime, with many locals enjoying the latest games on their consoles and computers.`, benefits: "Relaxation, Improved hand-eye coordination, Improved problem-solving skills, Improved decision-making abilities." },
+        { title: "Watching Anime", description: `Watching anime is a wonderful hobby that can be both educational and entertaining. Not only does it introduce you to different cultures and stories, but it also allows you to experience a range of emotions and develop empathy. In the Philippines, anime has become increasingly popular, with many locals enjoying the latest releases and classic shows.`, benefits: "Introduction to different cultures and stories, Improved empathy, Ability to experience a range of emotions." },
+        { title: "Reading Manga", description: `Reading manga is a fantastic hobby that can be both educational and entertaining. It provides an opportunity to escape into different worlds and experiences, and can also improve your reading and comprehension skills. In the Philippines, manga has become a popular form of entertainment, with many locals enjoying a wide range of genres.`, benefits: "Opportunity to escape into different worlds and experiences, Improved reading and comprehension skills." },
+        { title: "Adopting or Buying Pets", description: `Adopting or buying pets is a wonderful hobby that can bring joy and companionship into your life. Caring for a pet can also help improve your responsibility and organization skills, as well as increase your sense of empathy and love for animals. In the Philippines, pets have become an important part of many families, with many locals choosing to adopt or buy dogs, cats, and other animals.`, benefits: "Joy and companionship, Improved responsibility and organization skills, Increased sense of empathy and love for animals." }
+    ];
     return (
         <div className="scroll-smooth">
             <Head>
@@ -17,7 +19,7 @@ const Hobbies: NextPage = () => {
                 <link rel="icon" href="/CyberEd.gif" />
             </Head>
             <Navigation />
-            <div className="w-10/12 mx-auto p-5 bg-orange-100 md:w-2/3">
+            <div className="w-10/12 mx-auto p-5 bg-orange-100">
                 <div className="group text-center">
                     <div className="flex space-x-3 w-max h-max m-auto">
                         <svg className='fill-current text-blue-500 my-auto w-10 h-10' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M8 10.5c0 .827-.673 1.5-1.5 1.5s-1.5-.673-1.5-1.5.673-1.5 1.5-1.5 1.5.673 1.5 1.5zm16 6.216c0 1.587-.56 2.591-1.749 3.179-.143.071-.296.105-.449.105-.242 0-.482-.087-.672-.255l-3.109-2.745c-.558-.494-1.044-1.004-2.404-1.004h-7.233c-1.36 0-1.847.51-2.404 1.004l-3.11 2.745c-.188.168-.429.255-.672.255-.152 0-.307-.034-.449-.105-1.189-.588-1.749-1.592-1.749-3.178 0-2.062.945-5.461 2.681-9.857.822-2.083 2.292-2.86 3.695-2.86.656 0 1.298.17 1.853.456 2.424 1.249 5.17 1.223 7.544 0 .553-.286 1.195-.456 1.851-.456 1.403 0 2.874.777 3.696 2.86 1.735 4.395 2.68 7.794 2.68 9.856zm-15-6.216c0-1.381-1.119-2.5-2.5-2.5s-2.5 1.119-2.5 2.5 1.119 2.5 2.5 2.5 2.5-1.119 2.5-2.5zm4-2.5c0-.552-.447-1-1-1s-1 .448-1 1 .447 1 1 1 1-.448 1-1zm3.5 2.5c0-.415-.336-.75-.75-.75s-.75.335-.75.75.336.75.75.75.75-.335.75-.75zm1.75 1.75c0-.415-.336-.75-.75-.75s-.75.335-.75.75.336.75.75.75.75-.335.75-.75zm0-3.531c0-.415-.336-.75-.75-.75s-.75.335-.75.75.336.75.75.75.75-.336.75-.75zm1.734 1.781c0-.415-.336-.75-.75-.75s-.75.335-.75.75.336.75.75.75.75-.335.75-.75z"/></svg>
@@ -90,7 +92,7 @@ const Hobbies: NextPage = () => {
                     </ul>
                 </div>
             </div>
-            <div className="w-10/12 mx-auto p-5 bg-blue-100 md:w-2/3">
+            <div className="w-10/12 mx-auto p-5 bg-blue-100">
                 <div className="group text-center">
                     <div className="flex space-x-3 w-max h-max m-auto">
                         <svg className='fill-current text-blue-500 my-auto w-10 h-10' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M8 10.5c0 .827-.673 1.5-1.5 1.5s-1.5-.673-1.5-1.5.673-1.5 1.5-1.5 1.5.673 1.5 1.5zm16 6.216c0 1.587-.56 2.591-1.749 3.179-.143.071-.296.105-.449.105-.242 0-.482-.087-.672-.255l-3.109-2.745c-.558-.494-1.044-1.004-2.404-1.004h-7.233c-1.36 0-1.847.51-2.404 1.004l-3.11 2.745c-.188.168-.429.255-.672.255-.152 0-.307-.034-.449-.105-1.189-.588-1.749-1.592-1.749-3.178 0-2.062.945-5.461 2.681-9.857.822-2.083 2.292-2.86 3.695-2.86.656 0 1.298.17 1.853.456 2.424 1.249 5.17 1.223 7.544 0 .553-.286 1.195-.456 1.851-.456 1.403 0 2.874.777 3.696 2.86 1.735 4.395 2.68 7.794 2.68 9.856zm-15-6.216c0-1.381-1.119-2.5-2.5-2.5s-2.5 1.119-2.5 2.5 1.119 2.5 2.5 2.5 2.5-1.119 2.5-2.5zm4-2.5c0-.552-.447-1-1-1s-1 .448-1 1 .447 1 1 1 1-.448 1-1zm3.5 2.5c0-.415-.336-.75-.75-.75s-.75.335-.75.75.336.75.75.75.75-.335.75-.75zm1.75 1.75c0-.415-.336-.75-.75-.75s-.75.335-.75.75.336.75.75.75.75-.335.75-.75zm0-3.531c0-.415-.336-.75-.75-.75s-.75.335-.75.75.336.75.75.75.75-.336.75-.75zm1.734 1.781c0-.415-.336-.75-.75-.75s-.75.335-.75.75.336.75.75.75.75-.335.75-.75z"/></svg>
@@ -141,6 +143,20 @@ const Hobbies: NextPage = () => {
                         </li>
                     </ul>
                 </div>
+            </div>
+            <div className="bg-gray-300 bg-opacity-25 p-5 grid grid-cols-2 gap-3 mx-auto w-10/12">
+                {importanceHobbies.map((hobby, index) => (
+                    <div key={index} className="w-10/12 py-2 mx-auto">
+                    <div className="max-w-sm mx-auto">
+                        <div className="rounded overflow-hidden shadow-lg">
+                        <div className="px-6 py-4">
+                            <h3 className="text-lg font-bold text-center">{hobby.title}</h3>
+                            <p className="text-justify py-3">{hobby.description}</p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                ))}
             </div>
             <Footer />
         </div>
--- pages/skills.tsx
+++ pages/skills.tsx
@@ -10,6 +10,7 @@ import { Carousel } from 'react-responsive-carousel';
 const Skills: NextPage = () => {
 
     const designerskills = [
+        { name: "Adobe Animate", image: "/adobeAn.png" },
         { name: "Adobe XD", image: "/adobeXd.png" },
         { name: "Adobe Photoshop", image: "/adobePs.png" },
         { name: "Adobe Illustrator", image: "/adobeAi.png" },
@@ -29,7 +30,19 @@ const Skills: NextPage = () => {
         { name: "Material UI", image: "/materialUi.png" }
     ];
 
+    const techDescription = [  "Technical skills are essential for success in today's fast-paced and technology-driven job market. They are a combination of practical knowledge, hands-on experience, and expertise in using various tools and software programs. In order to be competitive, individuals must have a strong understanding of a wide range of technical skills that are relevant to their field. These skills can be divided into categories such as user experience design, graphic arts, web design, quality assurance, and game development.",  "User experience design involves creating digital products and interfaces that are both functional and user-friendly. This requires a deep understanding of human-computer interaction, accessibility, and user research. UX designers must have excellent problem-solving skills and be able to communicate their ideas effectively. They must be proficient in tools such as wireframing and prototyping software.",  "Graphic arts is the process of using digital and traditional media to communicate visual ideas and concepts. This field requires a strong eye for design, attention to detail, and proficiency in graphic design software. Graphic artists must be able to create images, logos, and other designs that are visually appealing and effective in conveying the intended message.",  "Web design involves creating websites that are aesthetically pleasing, user-friendly, and accessible to a wide range of users. Web designers must have a strong understanding of web technologies such as HTML, CSS, and JavaScript, as well as an understanding of user experience design principles. They must be able to create and test websites for different devices and screen sizes.",  "Quality assurance is a critical process in software development that involves testing and evaluating the functionality and performance of digital products. QA specialists must have strong analytical skills, attention to detail, and experience with testing tools and methodologies. They must be able to identify and report any bugs or issues with a product and work with developers to resolve them.",  "Associate unity game development involves using the Unity game engine to create interactive and engaging games. This field requires knowledge of programming languages such as C# and an understanding of game design principles. Associate unity game developers must have experience with game development tools and be able to create, test, and refine their games for optimal performance and user experience."];
+
     const unityskills = ["animation", "asset management", "audio", "editor interface", "game art principles", "game design principles", "industry awareness", "lighting", "materials and effects", "navigation and pathfinding", "physics", "programming", "project management", "user interface"];
+
+    const softSkills = [
+        { skill: 'Communication', description: 'Effective communication skills refer to the ability to convey information, ideas, and thoughts in a clear and concise manner to individuals or groups. It involves active listening, effective speaking, and writing skills.'},
+        { skill: 'Teamwork', description: 'Teamwork involves the ability to work collaboratively with others towards a common goal. It involves effective communication, cooperation, and a positive attitude towards resolving conflicts and working towards a solution.' },
+        { skill: 'Problem-solving', description: 'Problem-solving is the ability to identify, analyze, and find a solution to problems in an efficient and effective manner. It involves critical thinking, creativity, and a systematic approach to find the best solution.' },
+        { skill: 'Time management', description: 'Time management refers to the ability to prioritize tasks, set goals, and allocate time effectively to complete tasks efficiently. It involves planning, scheduling, and staying organized to meet deadlines.' },
+        { skill: 'Organizational skills', description: 'Organizational skills refer to the ability to effectively manage, prioritize, and complete tasks in a structured manner. It involves planning, time management, and the ability to stay organized in a fast-paced environment.'
+        },
+        { skill: 'Emotional intelligence', description: 'Emotional intelligence refers to the ability to recognize, understand, and manage one’s own emotions and the emotions of others. It involves empathy, self-awareness, and the ability to regulate emotions in a professional setting.' }
+    ];
 
     return (
         <div className={styles.container}>
@@ -41,15 +54,24 @@ const Skills: NextPage = () => {
 
             <main className='scroll-smooth'>
                 <h1 className='text-2xl uppercase text-red-500 font-bold my-3 text-center'>Technical Skill</h1>
+                <div className="w-10/12 py-2 mx-auto">
+                    {techDescription.map((skill, index) => (
+                        <p key={index} className="text-justify italic py-3">
+                        {skill}
+                        </p>
+                    ))}
+                </div>
                 <ul>
                     <li className='group'>
                         <div className="w-full h-auto grid grid-cols-1 grid-rows-2 justify-center items-center transition-all group-hover:bg-redg bg-dark border-b-2">
-                            <div className="flex justify-center items-center">
-                                <p className="group-hover:text-dark text-white mx-2">UI Designer Skills</p>
-                                <Image src='/CyberEd2.jpg' width={150} alt="" height={50} className='mx-2 rounded mb-5' />
+                        <div className="block text-center items-center md:flex lg:flex mx-auto">
+                            <p className="group-hover:text-dark text-center text-white mx-2">UI / Graphic Designer Skills</p>
+                            <div className="mx-auto">
+                                <Image src='/CyberEd2.jpg' width={150} alt="" height={50} className='md:mx-auto lg:mx-auto mx-0 my-0 w-auto rounded mb-5' />
                             </div>
+                        </div>
                             <div className="w-4/5 mx-auto mb-10 text-white group-hover:text-dark">
-                                <ul className='grid grid-cols-2 md:grid-cols-5 space-x-3 justify-center text-center'>
+                                <ul className='grid grid-cols-2 md:grid-cols-6 space-x-3 justify-center text-center'>
                                     {designerskills.map((designerskill, index) => {
                                         return (
                                         <li key={index} className='hover:animate-ping'>
@@ -89,7 +111,7 @@ const Skills: NextPage = () => {
                     <li className='group'>
                         <div className="w-full h-auto grid grid-cols-1 grid-rows-2 justify-center items-center transition-all group-hover:bg-redg bg-dark border-b-2">
                             <div className="w-max mx-auto my-0">
-                                <Image src='/unityBadge.png' width={150} alt="" height={150} className='mx-auto rounded' />
+                                <Image src='/unityBadge.png' width={300} alt="" height={300} className='mx-auto rounded' />
                             </div>
                             <div className="w-2/3 mx-auto sm:w-10/12 text-center gap-10 mb-10 text-white group-hover:text-dark">
                                 <ul className="grid grid-cols-1 md:grid-cols-3 text-center gap-5">
@@ -105,6 +127,15 @@ const Skills: NextPage = () => {
                         </div>
                     </li>
                 </ul>
+                <h1 className='text-2xl uppercase text-red-500 font-bold my-3 text-center'>Soft Skill</h1>
+                <div className="w-10/12 py-2 mx-auto">
+                    {softSkills.map((skill, index) => (
+                        <div key={index} className="py-3">
+                        <p className="text-justify italic font-medium">{skill.skill}</p>
+                        <p className="text-justify py-2">{skill.description}</p>
+                        </div>
+                    ))}
+                </div>
             </main>
 
             <Footer />
