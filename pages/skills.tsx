b8887c2ab23f79b9e07a11b9259dc4b688e16753
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2023-02-11 11:03:39 +0800

    update skills page

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -9,7 +9,25 @@ import { Carousel } from 'react-responsive-carousel';
 
 const Skills: NextPage = () => {
 
-    const designerskills = ["/adobeXd.png", "/adobePs.png", "/adobeAi.png", "/figma.png", "/balsamiq.png"];
+    const designerskills = [
+        { name: "Adobe XD", image: "/adobeXd.png" },
+        { name: "Adobe Photoshop", image: "/adobePs.png" },
+        { name: "Adobe Illustrator", image: "/adobeAi.png" },
+        { name: "Figma", image: "/figma.png" },
+        { name: "Balsamiq", image: "/balsamiq.png" }
+    ];
+
+    const webskills = [
+        { name: "HTML5", image: "/html5.png" },
+        { name: "CSS", image: "/css.png" },
+        { name: "Javascript", image: "/javascript.png" },
+        { name: "Php", image: "/php.png" },
+        { name: "MySQL", image: "/mysql.png" },
+        { name: "ReactJs", image: "/react.png" },
+        { name: "NextJs", image: "/next.png" },
+        { name: "Bootstrap", image: "/bootstrap.png" },
+        { name: "Material UI", image: "/materialUi.png" }
+    ];
 
     const unityskills = ["animation", "asset management", "audio", "editor interface", "game art principles", "game design principles", "industry awareness", "lighting", "materials and effects", "navigation and pathfinding", "physics", "programming", "project management", "user interface"];
 
@@ -36,9 +54,9 @@ const Skills: NextPage = () => {
                                         return (
                                         <li key={index} className='hover:animate-ping'>
                                             <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                            <Image src={designerskill} width={50} alt="" height={50} className='bg-transparent' />
+                                            <Image src={designerskill.image} width={50} alt="" height={50} className='bg-transparent' />
                                             </div>
-                                            <p className="">{designerskill}</p>
+                                            <p className="">{designerskill.name}</p>
                                         </li>
                                         );
                                     })}
@@ -46,128 +64,43 @@ const Skills: NextPage = () => {
                             </div>x
                         </div>
                     </li>
-                    <li className='group'>
+                    <li className="group">
                         <div className="bg-white border-b-2 grid grid-cols-1 grid-rows-2 group-hover:bg-redg h-auto items-center transition-all">
-                            <div className="h-auto items-center justify-center mx-auto w-max text-center">
-                                <p className="group-hover:text-white text-dark mx-2">Front-End Skills</p>
-                                <Image src='/CyberEd2.jpg' width={150} alt="" height={50} className='mx-2 rounded mb-5' />
-                            </div>
-                            <div className="w-2/3 md:w-4/5 mx-auto text-dark group-hover:text-white">
-                                <ul className='mb-10'>
-                                    <Carousel className="w-full" autoPlay infiniteLoop >
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/html5.png' width={50} alt="" height={50} />
-                                            </div>
-                                            <p className="">HTML5</p>
-                                        </li>
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/css.png' width={40} alt="" height={50} className='bg-transparent' />
-                                            </div>
-                                            <p className="">CSS</p>
-                                        </li>
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/javascript.png' width={50} alt="" height={50} className='bg-transparent' />
-                                            </div>
-                                            <p className="">Javascript</p>
-                                        </li>
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/php.png' width={80} alt="" height={50} className='bg-transparent' />
-                                            </div>
-                                            <p className="">Php</p>
-                                        </li>
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/mysql.png' width={80} alt="" height={50} className='bg-transparent' />
-                                            </div>
-                                            <p className="">MySQL</p>
-                                        </li>
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/react.png' width={50} alt="" height={50} className='bg-transparent' />
-                                            </div>
-                                            <p className="">ReactJs</p>
-                                        </li>
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/next.png' width={50} alt="" height={50} className='bg-transparent' />
-                                            </div>
-                                            <p className="">NextJs</p>
-                                        </li>
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/bootstrap.png' width={50} alt="" height={50} className='bg-transparent' />
-                                            </div>
-                                            <p className="">Bootstrap</p>
-                                        </li>
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/materialUi.png' width={70} alt="" height={50} className='bg-transparent' />
-                                            </div>
-                                            <p className="">Material Ui</p>
-                                        </li>
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/sass.png' width={70} alt="" height={50} className='bg-transparent' />
-                                            </div>
-                                            <p className="">SCSS / Sass</p>
-                                        </li>
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/tailwindcss.png' width={50} alt="" height={50} className='bg-transparent' />
-                                            </div>
-                                            <p className="">Tailwind CSS</p>
-                                        </li>
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/laravel.png' width={70} alt="" height={50} className='bg-transparent' />
-                                            </div>
-                                            <p className="">Laravel</p>
-                                        </li>
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/laravelJetstream.png' width={50} alt="" height={50} className='bg-transparent' />
-                                            </div>
-                                            <p className="">Jetstream</p>
-                                        </li>
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/laravelLivewire.png' width={50} alt="" height={50} className='bg-transparent' />
-                                            </div>
-                                            <p className="">Livewire</p>
-                                        </li>
-                                        <li className=' my-3 md:my-5'>
-                                            <div className='bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto'>
-                                                <Image src='/alpine.png' width={80} alt="" height={50} className='bg-transparent' />
-                                            </div>
-                                            <p className="">Alpine Js</p>
-                                        </li>
-                                    </Carousel>
-                                </ul>
-                            </div>
+                        <div className="h-auto items-center justify-center mx-auto w-max text-center">
+                            <p className="group-hover:text-white text-dark mx-2">Front-End Skills</p>
+                            <Image src="/CyberEd2.jpg" width={150} height={50} alt="logo" className="mx-2 rounded mb-5" />
+                        </div>
+                        <div className="w-2/3 md:w-4/5 mx-auto text-dark group-hover:text-white">
+                            <ul className="mb-10">
+                            <Carousel className="w-full" autoPlay infiniteLoop>
+                                {webskills.map((skill, index) => (
+                                <li key={index} className="my-3 md:my-5">
+                                    <div className="bg-transparent group-hover:bg-white p-5 rounded w-max mx-auto">
+                                    <Image src={skill.image} width={50} height={50} alt="web skill" />
+                                    </div>
+                                    <p className="">{skill.name}</p>
+                                </li>
+                                ))}
+                            </Carousel>
+                            </ul>
+                        </div>
                         </div>
                     </li>
                     <li className='group'>
                         <div className="w-full h-auto grid grid-cols-1 grid-rows-2 justify-center items-center transition-all group-hover:bg-redg bg-dark border-b-2">
-                            <div className="w-max mx-auto my-4">
-                                <Image src='/unityBadge.png' width={150} alt="" height={150} className='mx-auto rounded mb-5' />
+                            <div className="w-max mx-auto my-0">
+                                <Image src='/unityBadge.png' width={150} alt="" height={150} className='mx-auto rounded' />
                             </div>
-                            <div className="w-2/3 text-center gap-10 mx-auto mb-10 text-white group-hover:text-dark">
-                                <div className="w-max">
-                                    <p className="group-hover:text-dark text-white text-2xl uppercase bold mb-5">Skills</p>
-                                    <ul className="grid grid-cols-5 text-center gap-5">
-                                        {unityskills.map((skill, index) => {
-                                            return (
-                                                <li key={index} className="border-2 p-2 hover:animate-bounce rounded-md border-dashed hover:bg-white group-hover:shadow-lg border-white group-hover:border-solid ease-in-out duration-300">
-                                                <p className="capitalize text-lg">{skill}</p>
-                                                </li>
-                                            );
-                                        })}
-                                    </ul>
-                                </div>
+                            <div className="w-2/3 mx-auto sm:w-10/12 text-center gap-10 mb-10 text-white group-hover:text-dark">
+                                <ul className="grid grid-cols-1 md:grid-cols-3 text-center gap-5">
+                                    {unityskills.map((skill, index) => {
+                                        return (
+                                        <li key={index} className="flex items-center justify-center border-2 p-2 hover:animate-bounce rounded-md border-dashed hover:bg-white group-hover:shadow-lg border-white group-hover:border-solid ease-in-out duration-300 sm:p-4">
+                                            <p className="capitalize text-lg sm:text-xl">{skill}</p>
+                                        </li>
+                                        );
+                                    })}
+                                </ul>
                             </div>
                         </div>
                     </li>
