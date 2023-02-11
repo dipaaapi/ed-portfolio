9505b397b3b4e0fb65263f10f25dc2a2aea6370d
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2023-02-11 10:29:59 +0800

    update content in id

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,1 +1,16 @@
-export {};
+import React from 'react';
+
+interface Props {
+    id: string;
+}
+
+const User: React.FC<Props> = ({ id }) => {
+    return (
+        <div>
+        <h1>User {id}</h1>
+        <p>This is the user detail page for user with id: {id}</p>
+        </div>
+    );
+};
+
+export default User;
